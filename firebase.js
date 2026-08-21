7d5db0d6a802c7f4ebba65991d76db598391f587
bablu7352 <bablukumar8866bk@gmail.com>
2026-08-21 23:02:54 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,15 +1,20 @@
-import { initializeApp } from
+import {
+    initializeApp
+} from
 "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+
 
 import {
     getAuth
 } from
 "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
+
 import {
     getFirestore
 } from
 "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
 
 import {
     getStorage
@@ -21,26 +26,38 @@ const firebaseConfig = {
 
     apiKey: "YOUR_API_KEY",
 
-    authDomain: "YOUR_PROJECT.firebaseapp.com",
+    authDomain:
+        "YOUR_PROJECT.firebaseapp.com",
 
-    projectId: "YOUR_PROJECT_ID",
+    projectId:
+        "YOUR_PROJECT_ID",
 
-    storageBucket: "YOUR_PROJECT.firebasestorage.app",
+    storageBucket:
+        "YOUR_PROJECT.firebasestorage.app",
 
-    messagingSenderId: "YOUR_SENDER_ID",
+    messagingSenderId:
+        "YOUR_SENDER_ID",
 
-    appId: "YOUR_APP_ID"
+    appId:
+        "YOUR_APP_ID"
 
 };
 
 
-const app = initializeApp(firebaseConfig);
+const app =
+    initializeApp(firebaseConfig);
 
-const auth = getAuth(app);
 
-const db = getFirestore(app);
+const auth =
+    getAuth(app);
 
-const storage = getStorage(app);
+
+const db =
+    getFirestore(app);
+
+
+const storage =
+    getStorage(app);
 
 
 export {
